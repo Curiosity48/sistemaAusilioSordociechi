259de1a522a650a498cb7d3ec803f891e41108c1
peduzzi_samuele <peduzzi_samuele@LABINFO1-01.jeanmonnet.local>
2020-01-08 09:55:00 +0100

    HTTP request method

--- JServerSordociechi/src/jserversordociechi/JMaps.java
+++ JServerSordociechi/src/jserversordociechi/JMaps.java
@@ -5,24 +5,37 @@
  */
 package jserversordociechi;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author peduzzi_samuele
  */
 public class JMaps {
-    
-   private GeoPosition position;
+
+    private GeoPosition position;
 
     public JMaps() {
-        
-        position = new GeoPosition(0,0);
-    }
-    
-    
 
-    
-   
-   
-   
-    
+        position = new GeoPosition(0, 0);
+    }
+
+    public void sendGoogleMapsHttpRequest(String requestString) {
+
+        try {
+            URL url;
+            url = new URL(requestString);
+            HttpsConnection conn = (HttpsConnection) url.openConnection();
+            InputStream stream = conn.getInputStream();
+        } 
+        catch (Exception ex) {
+            Logger.getLogger(JMaps.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
