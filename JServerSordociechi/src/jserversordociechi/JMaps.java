ae4485fc9a9492e49dbad90d448784bd5a9688e5
peduzzi_samuele <peduzzi_samuele@LABINFO1-01.jeanmonnet.local>
2020-01-08 09:39:55 +0100

    Introduzione GeoPosition

---
+++ JServerSordociechi/src/jserversordociechi/JMaps.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jserversordociechi;
+
+/**
+ *
+ * @author peduzzi_samuele
+ */
+public class JMaps {
+    
+   private GeoPosition position;
+
+    public JMaps() {
+        
+        position = new GeoPosition(0,0);
+        
+    }
+   
+   
+   
+    
+}
