10ff8b734a89e5a7afd6c24116602dbc60cc372c
Brozio01 <47627888+Brozio01@users.noreply.github.com>
2020-01-10 12:15:32 +0100

    Add files via upload

---
+++ testImageReceive/testImageReceive/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace testImageReceive
+{
+    public partial class Form1 : Form
+    {
+        private MyTcpListener tcp;
+        private Thread t;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            tcp = new MyTcpListener(pictureBox1);
+            t = new Thread(new ThreadStart(tcp.ReceiveImg));
+            tcp.Connect();
+            t.Start();
+        }
+    }
+}
---
+++ testImageReceive/testImageReceive/MyTcpListener.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace testImageReceive
+{
+    class MyTcpListener
+    {
+        private TcpClient client;
+        private TcpListener server;
+        private NetworkStream stream;
+        private byte[] data;
+        private PictureBox box;
+
+        public MyTcpListener(PictureBox box)
+        {
+            this.box = box;
+            Int32 porta = 5000;
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            server = new TcpListener(address, porta);
+            server.Start();
+            data = new byte[1024];
+        }
+
+        public void Connect()
+        {
+            //inizializzazione connessione
+            client = server.AcceptTcpClient();
+            stream = client.GetStream();
+        }
+
+        public void ReceiveImg()
+        {
+            //la connessione finisce quando il client la chiude
+            while (client.Connected)
+            {
+                //ricezione dimensione immagine in byte[] e riallocazione vettore
+                stream.Read(data, 0, data.Length);    //num celle occupate di data (in data è contenuto la grandezza in byte dell'immagine)
+                int dim = BitConverter.ToInt32(data, 0);
+                data = new byte[dim];
+
+                //ricezione immagine in byte[] e conversione
+                stream.Read(data, 0, data.Length);    //adesso in data è contenuta l'immagine in formato byte
+                Image finalImg = (Image)((new ImageConverter()).ConvertFrom(data));
+                System.IO.File.WriteAllBytes("C:\\test\\tmp.jpg", data);
+            }
+            Close();
+        }
+
+        public void Close()
+        {
+            stream.Close();
+            client.Close();
+            server.Stop();
+        }
+    }
+}
