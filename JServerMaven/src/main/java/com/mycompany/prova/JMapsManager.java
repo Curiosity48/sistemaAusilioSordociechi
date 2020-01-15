0c74246e6e5251e7c4a4f003ac48a6b440f0b80b
peduzzi_samuele <peduzzi_samuele@LABINFO1-02.jeanmonnet.local>
2020-01-15 09:12:01 +0100

    Maven

---
+++ JServerMaven/src/main/java/com/mycompany/prova/JMapsManager.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prova;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author peduzzi_samuele
+ */
+public class JMapsManager {
+
+    private GeoPosition position;
+
+    public JMapsManager() {
+
+        position = new GeoPosition(0, 0);
+    }
+
+    
+
+}
